3197d05c7a3c4363102117b6f3f99dc8c67c40a5
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2024-07-20 16:00:54 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -12,12 +12,6 @@ php_exec() {
       php${phpversion} "$install_dir/bin/console" --no-interaction --env=prod "$@")
 }
 
-# Define a function to execute commands with `php_exec`
-php_exec() {
-  (cd "$install_dir" && ynh_exec_as "$app" \
-      php${phpversion} "$install_dir/bin/console" --no-interaction --env=prod "$@")
-}
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
